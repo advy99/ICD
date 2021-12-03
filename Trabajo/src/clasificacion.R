cc06a2d59dd6eb4f545e92352d3e6f4a09064399
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-12-03 13:42:10 +0100

    normalizamos los datos y separamos en train/test con 80/20

--- Trabajo/src/clasificacion.R
+++ Trabajo/src/clasificacion.R
@@ -16,6 +16,27 @@ str(iris)
 iris$Class <- as.factor(iris$Class)
 str(iris)
 
-train(iris)
+# aunque las medidas de iris están en las mismas unidades, y son relativamente 
+# similares, aun así vamos a normalizarlas ya que no sabemos a priori si alguna
+# tendrá más peso al clasificar con knn, de forma que todas tengan la misma importancia
+
+iris <- iris %>% mutate_if(is.numeric, scale, center = TRUE, scale = TRUE)
+
+summary(iris)
+
+# separamos en training y test (90 / 10)
+PORCENTAJE_TEST = 0.2
+muestras_train <- sample(nrow(iris), nrow(iris) * (1 - PORCENTAJE_TEST))
+
+# aprovechamos y le quitamos la clase
+iris_train <- iris[muestras_train, -5]
+iris_test <- iris[-muestras_train, -5]
+
+nrow(iris_train)
+nrow(iris_test)
+
+iris_train_etiquetas <- iris[muestras_train, 5]
+iris_test_etiquetas <- iris[-muestras_train, 5]
+
 
 
